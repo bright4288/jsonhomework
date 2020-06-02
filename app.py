fb4f4bbf61bbaa34d77572d8608e424580c68a41
강유진 <alsl4288@naver.com>
2020-06-02 14:38:24 +0900

    로그인처리 모름

--- app.py
+++ app.py
@@ -1,52 +1,52 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session,abort
+
 app = Flask(__name__)
 
-import game
-import json
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if not session.get('logged_in'):
+		return render_template('index.html')
+	else:
+		if request.method == 'POST':
+			username = request.form['username']
+			return render_template('index.html', data=username)
+		return render_template('index.html')
 
-@app.route('/hello')
-def hellohtml():
-    return render_template("hello.html")
-
-@app.route('/hello/<name>')
-def hellovar(name):
-    character = game.set_charact(name)
-    return "{0}님 반갑습니다. (HP {1})으로 게임을 시작 합니다.".format(character["name"],character["hp"])
-
-
-@app.route('/form')
-def form():
-    return render_template('test.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	"""Login Form"""
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		name = request.form['username']
+		passw = request.form['password']
+		try:
+			data = User.query.filter_by(username=name, password=passw).first()
+			if data is not None:
+				session['logged_in'] = True
+				return redirect(url_for('home'))
+			else:
+				return '로그인 안됨'
+		except:
+			return "로그인 다시하시오"
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
-        # args_dict = request.args.to_dict()
-        # print(args_dict)
-        num = request.args["num"]
-        name = request.args.get("name")
-
-        return "GET으로 전달된 데이터({}, {})".format(num, name)
+        return 'GET 으로 전송이다.'
     else:
-        num = request.form["num"]
-        name = request.form["name"]
-        with open("static/save.txt","w", encoding='utf-8') as f:
-            f.write("%s,%s" % (num, name))
-        return "POST로 전달된 데이터({}, {})".format(num, name)
+        name = request.form["username"]
+        passw = request.form["password"]
+        return 'POST 이다. 학번은: {} 이름은: {}'.format(name, passw)
 
-@app.route('/getinfo')
-def getinfo():
-    # 파일 입력
-    with open("static/save.txt", "r", encoding='utf-8') as file:
-        student = file.read().split(',')  # 쉽표로 잘라서 student 에 배열로 저장
-    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
 
-if __name__ == '__main__': 
+@app.route("/logout")
+def logout():
+	"""Logout Form"""
+	session['logged_in'] = False
+	return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.secret_key = b'aaa!111/'
     app.run(debug=True)
-
-
-
